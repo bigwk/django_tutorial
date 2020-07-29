e6f4892e01198759c9057f4a9ea2c000610f9a46
bigwk <ohmykui@gmail.com>
2020-07-29 23:53:44 +0800

    modify views

--- events/urls.py
+++ events/urls.py
@@ -20,5 +20,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='events'),
+    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     # path('admin/', admin.site.urls),
 ]
--- events/views.py
+++ events/views.py
@@ -9,3 +9,14 @@ def index(request):
     # request_str = request.headers
     # request_str = request.read()
     return HttpResponse(request_str)
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' % question_id)
+
+def results(request, question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = 'You are voting on question {}'.format(question_id)
+    return HttpResponse(response)
