06d0ce69b0e122a2f32b7af72d60bfea25b3fbe2
bigwk <ohmykui@gmail.com>
2020-07-30 12:12:10 +0800

    use template

--- events/urls.py
+++ events/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'events'
 urlpatterns = [
-    path('', views.index, name='events'),
+    path('', views.index, name='index'),
     path('<int:question_id>', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
--- events/views.py
+++ events/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    request_str = str(type(request))
-    requests = dir(request)
-    request_str += '。'.join(requests)
-    # request_str = request.headers
-    # request_str = request.read()
-    return HttpResponse(request_str)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('events/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = '<br> '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at question %s.' % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question Does Not Exist.')
+    # question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'events/detail.html', context={'question': question})
 
 def results(request, question_id):
-    response = 'You are looking at the results of question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # response = 'You are looking at the results of question %s.'
+    return render(request, 'events/results.html', context={'question': question})
 
 def vote(request, question_id):
     response = 'You are voting on question {}'.format(question_id)
