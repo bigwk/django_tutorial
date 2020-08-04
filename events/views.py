8610181b50cddd1dc05f12867f5c37b95d4a8aec
bigwk <ohmykui@gmail.com>
2020-08-04 17:14:23 +0800

    use generic view

--- events/views.py
+++ events/views.py
@@ -3,11 +3,31 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
 # Create your views here.
+# generic views
+class IndexView(generic.ListView):
+    template_name = 'events/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'events/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'events/results.html'
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('events/index.html')
@@ -25,10 +45,12 @@ def detail(request, question_id):
     # question = get_object_or_404(Question, pk=question_id)
     return render(request, 'events/detail.html', context={'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     # response = 'You are looking at the results of question %s.'
     return render(request, 'events/results.html', context={'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
